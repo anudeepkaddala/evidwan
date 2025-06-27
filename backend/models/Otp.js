6281f7d2745f50de90bb7cdd8e1600113e85ed9b
anudeepkaddala <142006783+anudeepkaddala@users.noreply.github.com>
2025-06-27 10:51:20 +0530

    Delete backend directory

--- backend/models/Otp.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const otpSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true }, // Email for which OTP is generated
-  otp: { type: Number, required: true }, // Generated OTP
-  otpExpires: { type: Date, required: true }, // Expiry time for the OTP
-});
-
-module.exports = mongoose.model("Otp", otpSchema);
